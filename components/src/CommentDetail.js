1c196dbfd0406f98b1b39cab81a498a874631873
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2019-01-05 10:08:33 -0800

    Created CommentDetail component.

---
+++ components/src/CommentDetail.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import faker from 'faker';
+ 
+const CommentDetail = () => {
+    return (
+    <div className="comment">
+        <a href="/" className="avatar">
+            <img src={faker.image.avatar()} alt="avatar"/>
+        </a>
+        <div className="content">
+            <a href="/" className="author">
+                {faker.name.firstName()}
+            </a>
+            <div className="metadata">
+                <span className="date">Today at 5:00PM</span>
+            </div>
+            <div className="text">
+                {faker.random.words(5)}
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default CommentDetail;
+
--- components/src/index.js
+++ components/src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import CommentDetail from './CommentDetail';
+
 const App = () => {
-    return (
-        <div>Hello</div>
-    );
+	return (
+		<div className="ui container comments">
+			<CommentDetail />
+			<CommentDetail />
+			<CommentDetail />
+		</div>
+	);
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
