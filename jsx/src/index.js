56a01aa199fa6f8c5a009f0337e581e4cb76a85f
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2019-01-05 09:09:26 -0800

    Section 1 and 2 done. Created component using JSX.

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,7 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return <div>Hello world!</div>;
+    
+    const textButton = { text: 'GET TIME!'};
+    const style = { backgroundColor: "blue", border: "1px solid red", color: "white", cursor: "pointer" }
+    function getTime() {
+        return (new Date()).toLocaleTimeString()
+    }
+
+    // let currentTime = getTime();
+
+    return (
+        <div>
+            <label className="label">
+                Enter name:
+            </label>
+            <input id="name" type="text"/>
+            <h1>{ getTime() }</h1>
+            <button style={style}>
+                {textButton.text}
+            </button>
+        </div>
+    )
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
